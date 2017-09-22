637373904af851a5b1ee6db789e4415426447170
oglivie6720 <carlosogliviedrummond@gmail.com>
2017-09-21 21:12:42 -0500

    Practica Numero uo de Java

---
+++ jdk9 class1/src/pa/edu/uip/Main.java
@@ -0,0 +1,28 @@
+package pa.edu.uip;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Bienvenidos al Mundo Java!");
+	//TIPOS DE ENTEROS
+
+	byte d; //8bits
+	short b; //16bits
+	int a = 0; //32bits
+	long c; // 64bits
+
+    //TIPOS DE DECIMALES
+    float e = 0.0f; //32 bits mientras se declara no se le escribe "f"
+    double f = 0.0; //64 bits
+
+    //CARACTERES
+    char g = 'a';
+    //CADENAS
+    String h = "a";
+
+    //BOOLEANO
+    boolean i = true;
+    i = false;
+        System.out.println(a);
+    }
+}
